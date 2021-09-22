2c9ff17d66493083dd3b418204f44a7cc90c8250
Laura Sofia Hernandez Trillos <88508324+LauSofiTrillos@users.noreply.github.com>
2021-09-22 12:12:41 -0500

    Create enum.cpp

---
+++ enum.cpp
@@ -0,0 +1,11 @@
+// ejemplo enumeracion 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  enum days_of_week { monday = 'M', tuesday = 'T', friday = 'F' };
+  days_of_week day = monday;
+  cout << (char)day << endl;
+}
